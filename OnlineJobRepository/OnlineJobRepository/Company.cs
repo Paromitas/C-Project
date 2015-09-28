e253f88907f9b99404e8438b17a65ee237a7a2c0
Paromita Sen <v-pasen@microsoft.com>
2015-09-27 17:33:47 -0700

    Updated Classes

--- OnlineJobRepository/OnlineJobRepository/Company.cs
+++ OnlineJobRepository/OnlineJobRepository/Company.cs
@@ -8,5 +8,10 @@ namespace OnlineJobRepository
 {
     class Company
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Profile { get; set; }
+        public Boolean Hiring { get; set; }
     }
 }
