c07f78f950e05402d620b98d1af71ae64da15110
Paromita Sen <v-pasen@microsoft.com>
2015-09-28 18:37:30 -0700

    Adding User Class

--- OnlineJobRepository/OnlineJobRepository/User.cs
+++ OnlineJobRepository/OnlineJobRepository/User.cs
@@ -10,7 +10,7 @@ namespace OnlineJobRepository
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Address { get; set; }
+        public string Contact { get; set; }
         public string Profile { get; set; }
         public string Skill { get; set; }
 
